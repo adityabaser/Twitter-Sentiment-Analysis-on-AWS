45289019ad2786321443944674b0b66808bdb420
Umesh Bodhwani <umeshroxcent@gmail.com>
2020-03-04 22:15:14 -0500

    add zip file path

--- twitter_file.py
+++ twitter_file.py
@@ -29,18 +29,18 @@ class TwitterClass:
 
 
         #importing word_list
-        file_path = './word_list.txt'
-        # if ".zip/" in file_path:
-        #     archive_path = os.path.abspath(file_path)
-        #     split = archive_path.split(".zip/")
-        #     archive_path = split[0] + ".zip"
-        #     path_inside = split[1]
-        #     archive = zipfile.ZipFile(archive_path, "r")
-        #     self.embeddings = archive.read(path_inside).decode("utf8").split("\n")
-        #     self.embeddings = self.embeddings[:max_length_dictionary]
+        file_path = './Preprocessing.zip/word_list.txt'
+        if ".zip/" in file_path:
+            archive_path = os.path.abspath(file_path)
+            split = archive_path.split(".zip/")
+            archive_path = split[0] + ".zip"
+            path_inside = split[1]
+            archive = zipfile.ZipFile(archive_path, "r")
+            self.embeddings = archive.read(path_inside).decode("utf8").split("\n")
+            self.embeddings = self.embeddings[:max_length_dictionary]
 
-        # else:
-        self.embeddings = open(file_path, 'r', encoding="utf-8").read().split("\n")
+        else:
+            self.embeddings = open(file_path, 'r', encoding="utf-8").read().split("\n")
 
         self.tokenizer = TweetTokenizer()
 
@@ -52,22 +52,22 @@ class TwitterClass:
         Remove stopwords
         """
 
-        file_path = './english'
-        # if ".zip/" in file_path:
-        #     archive_path = os.path.abspath(file_path)
-        #     split = archive_path.split(".zip/")
-        #     archive_path = split[0] + ".zip"
-        #     path_inside = split[1]
-        #     archive = zipfile.ZipFile(archive_path, "r")
-        #     stopwords = archive.read(path_inside).decode("utf8").split("\n")
+        file_path = './Preprocessing.zip/english'
+        if ".zip/" in file_path:
+            archive_path = os.path.abspath(file_path)
+            split = archive_path.split(".zip/")
+            archive_path = split[0] + ".zip"
+            path_inside = split[1]
+            archive = zipfile.ZipFile(archive_path, "r")
+            stopwords = archive.read(path_inside).decode("utf8").split("\n")
 
-        # else:
-        stopwords = []
-        with open("english") as files:
-            for line in files:
-                values = line.split()
-                word = values[0]
-                stopwords.append(word)
+        else:
+            stopwords = []
+            with open("english") as files:
+                for line in files:
+                    values = line.split()
+                    word = values[0]
+                    stopwords.append(word)
         # stop_words = set(stopwords.words('english'))
         pattern = re.compile(r'\b(' + r'|'.join(stopwords) + r')\b\s*')
         tweet = pattern.sub('', tweet)
@@ -140,16 +140,16 @@ class TwitterClass:
 
         return token_pad
 
-    # def processed(self, tweet):
-    #     """
-    #     Function to return final output
-    #     """
-    #     cleaned = self.clean_text(tweet)
-    #     tokens = self.tokenize_text(cleaned)
-    #     embeddings = self.replace_token_with_index(tokens)
-    #     padded = self.pad_sequence(embeddings)
+    def processed(self, tweet):
+        """
+        Function to return final output
+        """
+        cleaned = self.clean_text(tweet)
+        tokens = self.tokenize_text(cleaned)
+        embeddings = self.replace_token_with_index(tokens)
+        padded = self.pad_sequence(embeddings)
 
-    #     return padded
+        return padded
 
 
 
